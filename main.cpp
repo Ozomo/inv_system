e13d0c9f7e1c579ddb0d2b7a08f14a9e26751268
Ozomo <emil000320@gmail.com>
2025-12-22 14:30:40 +0100

    .

--- main.cpp
+++ main.cpp
@@ -2,10 +2,29 @@
 #include <vector>
 #include <fstream>
 #include <algorithm>
+#include <string>
 using namespace std;
 
-void clearing_vector(vector <vector<int>>& vec) { //ready
+void sort(vector <vector<int>>& inventory, vector <string>& item_names) {
+    // Simple bubble sort to keep item_names in sync with inventory
+    for (int i = 0; i < inventory.size(); i++) {
+        for (int j = 0; j < inventory.size() - i - 1; j++) {
+            if (inventory[j][0] > inventory[j + 1][0]) {
+                swap(inventory[j], inventory[j + 1]);
+                swap(item_names[j], item_names[j + 1]);
+            }
+        }
+    }
+}
+
+void clearing_vector_int(vector <vector<int>>& vec) { //ready
     while(vec.size() > 0){
+        vec.pop_back();
+    }
+}
+
+void clearing_vector_string(vector <string>& vec) { //ready
+    while(vec.size() >0){
         vec.pop_back();
     }
 }
@@ -20,7 +39,7 @@ bool is_there_already(vector <vector<int>>& inventory, int item) { //ready
     return false;
 }
 
-void add_item(vector <vector<int>>& inventory, int item) { //ready
+void add_item(vector <vector<int>>& inventory, vector <string>& item_names, int item) { //ready
     if(is_there_already(inventory, item)){
         for(int i = 0; i < inventory.size(); i++){
             if(inventory[i][0] == item){
@@ -29,10 +48,31 @@ void add_item(vector <vector<int>>& inventory, int item) { //ready
             }
         }
     } else {
+        item_names.push_back("Unnamed Item");
         inventory.push_back({item, 1});
     }
 }
 
+void add_name(vector <vector<int>>& inventory, vector <string>& item_names, int item, string name) {
+    if(!is_there_already(inventory, item)){ //check if item exists
+        cout<<"Item does not exist in inventory."<<endl;
+        return;
+    }
+
+    for(int i = 0; i < inventory.size(); i++){
+        if(inventory[i][0] == item){
+            if(i < item_names.size()){
+                item_names[i] = name;
+            } 
+            else {
+                item_names.push_back(name);
+            }
+            cout<<"Name ["<<name<<"] assigned to item ["<<item<<"]."<<endl;
+            return;
+        }
+    }
+
+}
 void remove_quantity(vector <vector<int>>& inventory, int item) { //ready
     for(int i = 0; i < inventory.size(); i++){
         if(inventory[i][0] == item && inventory[i][1] > 0){
@@ -74,15 +114,15 @@ void remove_item(vector <vector<int>>& inventory, int item) { //ready
     }
 }
 
-void show_items(vector <vector<int>>& inventory) { //ready
-    sort(inventory.begin(), inventory.end());
+void show_items(vector <vector<int>>& inventory, vector <string>& item_names) { //ready
+    sort(inventory, item_names);
     cout << "Items in inventory:" << endl;
     for(int i = 0; i < inventory.size(); i++){
-        cout << "Item: " << inventory[i][0] << " | Quantity: " << inventory[i][1] << endl;
+        cout << "Item: " << inventory[i][0] << " | Name: " << item_names[i] <<" | Quantity: " << inventory[i][1] << endl;
     }
 }
 
-void reading(vector <vector<int>>& inventory){ //ready
+void reading(vector <vector<int>>& inventory, vector <string>& item_names){ //ready
     fstream file;
     file.open("inventory.txt", ios::in);
 
@@ -91,8 +131,8 @@ void reading(vector <vector<int>>& inventory){ //ready
         return;
     }
 
-    clearing_vector(inventory);
-
+    clearing_vector_int(inventory);
+    clearing_vector_string(item_names);
     string line;
     int item;
     int qty;
@@ -104,18 +144,21 @@ void reading(vector <vector<int>>& inventory){ //ready
         }   
         item = stoi(line);
         file>>line;
+        item_names.push_back(line);
+        file>>line;
         qty = stoi(line);
         inventory.push_back({item, qty});
         line = "";
     }
 }
 
-void writing(vector <vector<int>>& inventory){ //ready
+void writing(vector <vector<int>>& inventory, vector <string> item_names){ //ready
     fstream file;
     file.open("inventory.txt", ios::out | ios::trunc);
 
     for(int i = 0; i < inventory.size(); i++){
         file<<inventory[i][0]<<endl;
+        file<<item_names[i]<<endl;
         file<<inventory[i][1]<<endl;
     }
 }
@@ -125,17 +168,19 @@ void menu(){ //ready
     cout<<"-----------------------------"<<endl;
     cout<<"1. Fast Add Item"<<endl;
     cout<<"2. Add Item"<<endl;
-    cout<<"3. Remove Quantity"<<endl;
-    cout<<"4. Remove Item"<<endl;
-    cout<<"5. Show Items"<<endl;
-    cout<<"6. Save Inventory to File"<<endl;
-    cout<<"7. Load Inventory from File"<<endl;
-    cout<<"8. Exit"<<endl;
+    cout<<"3. Name Item"<<endl;
+    cout<<"4. Remove Quantity"<<endl;
+    cout<<"5. Remove Item"<<endl;
+    cout<<"6. Show Items"<<endl;
+    cout<<"7. Save Inventory to File"<<endl;
+    cout<<"8. Load Inventory from File"<<endl;
+    cout<<"9. Exit"<<endl;
     cout<<"-----------------------------"<<endl;
 }
 
 int main(){
     vector <vector<int>> inventory = {};
+    vector <string> item_names = {};
 
     menu();
 
@@ -152,7 +197,7 @@ int main(){
             int item_to_add;
             cout<<"Enter item ID to add: ";
             cin>>item_to_add;
-            add_item(inventory, item_to_add);
+            add_item(inventory, item_names,item_to_add);
             system("cls");
             menu();
             cout<<"Item ["<<item_to_add<<"] added successfully."<<endl;
@@ -168,7 +213,7 @@ int main(){
             cout<<"Enter quantity to add: ";
             cin>>qty;
             for(int i = 0; i < qty; i++){
-                add_item(inventory, item_to_add);
+                add_item(inventory, item_names, item_to_add);
             }
             system("cls");
             menu();
@@ -176,6 +221,21 @@ int main(){
             break;
         }
         case 3:{
+            system("cls");
+            menu();
+            cout<<"Enter item ID to name it: ";
+            int item;
+            cin>>item;
+            cout<<"Enter name for the item: ";
+            string name;
+            cin.ignore();
+            cin>>name;
+            system("cls");
+            menu();
+            add_name(inventory, item_names, item, name);
+            break;
+        }
+        case 4:{
             system("cls");
             menu();
             int item_to_remove_qty;
@@ -186,7 +246,7 @@ int main(){
             remove_quantity(inventory, item_to_remove_qty);
             break;
         }
-        case 4:{
+        case 5:{
             system("cls");
             menu();
             int item_to_remove;
@@ -197,27 +257,27 @@ int main(){
             remove_item(inventory, item_to_remove);
             break;
         }
-        case 5:{
-            system("cls");
-            menu();
-            show_items(inventory);
-            break;
-        }
         case 6:{
             system("cls");
             menu();
-            writing(inventory);
-            cout<<"Inventory saved to file."<<endl;
+            show_items(inventory, item_names);
             break;
         }
         case 7:{
             system("cls");
             menu();
-            reading(inventory);
-            cout<<"Inventory loaded from file."<<endl;
+            writing(inventory, item_names);
+            cout<<"Inventory saved to file."<<endl;
             break;
         }
         case 8:{
+            system("cls");
+            menu();
+            reading(inventory, item_names);
+            cout<<"Inventory loaded from file."<<endl;
+            break;
+        }
+        case 9:{
             return 0;
             break;
         }
